c242e510de663ace0117b3094ac606cf6a9e6b5f
Wayne Ashley Berry <727262+wayneashleyberry@users.noreply.github.com>
2025-01-31 08:06:12 +0200

    more rust

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::fs;
 use std::path::{Path, PathBuf};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let matches = Command::new("gitroot")
+    let mut app = Command::new("gitroot")
         .about("Find and move to your git root directory")
         .subcommand(
             Command::new("find")
@@ -24,9 +24,12 @@ fn main() -> Result<(), Box<dyn Error>> {
                 .about("Initialise your shell")
                 .subcommand(Command::new("fish").about("Initialize Fish shell"))
                 .subcommand(Command::new("bash").about("Not yet implemented"))
-                .subcommand(Command::new("zsh").about("Not yet implemented")),
-        )
-        .get_matches();
+                .subcommand(Command::new("zsh").about("Not yet implemented"))
+                .subcommand(Command::new("nu").about("Not yet implemented"))
+                .subcommand(Command::new("elvish").about("Not yet implemented")),
+        );
+
+    let matches = app.clone().get_matches();
 
     match matches.subcommand() {
         Some(("find", sub_matches)) => {
@@ -36,23 +39,39 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
         Some(("init", sub_matches)) => match sub_matches.subcommand_name() {
             Some("fish") => println!("alias gr=\"cd $(gitroot find --match last)\""),
-            Some("bash") | Some("zsh") => return Err("Not yet implemented".into()),
-            _ => {}
+            Some("bash") | Some("zsh") | Some("nu") | Some("elvish") => {
+                return Err("Not yet implemented".into())
+            }
+            Some(_) => return Err("Unsupported shell specified".into()),
+            None => {
+                let mut init_cmd = Command::new("init")
+                    .about("Initialise your shell")
+                    .subcommand(Command::new("fish").about("Initialize Fish shell"))
+                    .subcommand(Command::new("bash").about("Not yet implemented"))
+                    .subcommand(Command::new("zsh").about("Not yet implemented"))
+                    .subcommand(Command::new("nu").about("Not yet implemented"))
+                    .subcommand(Command::new("elvish").about("Not yet implemented"));
+                init_cmd.print_help()?;
+            }
         },
-        _ => {}
+        _ => {
+            app.print_help()?;
+        }
     }
-    
+
     Ok(())
 }
 
 fn find_git_dir(start_dir: &Path, find_first: bool) -> String {
     let mut match_path: Option<PathBuf> = None;
     let mut current_dir = start_dir.to_path_buf();
-    
+
     while current_dir.parent().is_some() {
         if let Ok(entries) = fs::read_dir(&current_dir) {
             for entry in entries.flatten() {
-                if entry.file_name() == ".git" && entry.file_type().map(|t| t.is_dir()).unwrap_or(false) {
+                if entry.file_name() == ".git"
+                    && entry.file_type().map(|t| t.is_dir()).unwrap_or(false)
+                {
                     match_path = Some(current_dir.clone());
                     if find_first {
                         return match_path.unwrap().to_string_lossy().into_owned();
@@ -60,13 +79,16 @@ fn find_git_dir(start_dir: &Path, find_first: bool) -> String {
                 }
             }
         }
-        
+
         if let Some(parent) = current_dir.parent() {
             current_dir = parent.to_path_buf();
         } else {
             break;
         }
     }
-    
-    match_path.unwrap_or(start_dir.to_path_buf()).to_string_lossy().into_owned()
+
+    match_path
+        .unwrap_or(start_dir.to_path_buf())
+        .to_string_lossy()
+        .into_owned()
 }
